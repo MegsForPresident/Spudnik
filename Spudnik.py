2a53016a0081d1d8d0f5116ba6ccbacdb4960b78
Pramit Majumder <rhijusudipta@gmail.com>
2021-12-29 12:29:41 +0530

    Done

--- Spudnik.py
+++ Spudnik.py
@@ -590,5 +590,6 @@ async def on_message(message):
 for fileName in os.listdir('./cogs'):
     if fileName.endswith('.py'):
         client.load_extension(f'cogs.{fileName[:-3]}') 
-# client.run('ODcwNjY5Mjc2ODcxMjYyMjE4.YQQH8w.Od5lsYmpsJ9BnvHrWzIZOYSdRFE') unban
+client.run('ODcwNjY5Mjc2ODcxMjYyMjE4.YQQH8w.KLGwLzpkwysYnnXGKn8lvRxdEOA')
+# client.run('OTEyMzc2NzUwODYyODkzMTI2.YZvDEA.kXZqdfhiFxBxYw5P8lFjcZ-ZWsQ')
 # ⬢
