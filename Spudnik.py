fd934ce610c991e09a55937d3c969b09f9d3990d
Pramit Majumder <rhijusudipta@gmail.com>
2022-01-02 21:50:43 +0530

    Done

--- Spudnik.py
+++ Spudnik.py
@@ -611,5 +611,5 @@ for fileName in os.listdir('./cogs'):
     if fileName.endswith('.py'):
         client.load_extension(f'cogs.{fileName[:-3]}') 
 
-client.run('ODcwNjY5Mjc2ODcxMjYyMjE4.YQQH8w.jR_s-zy7gTt5arHPufsK_5p7prA')
+client.run('ODcwNjY5Mjc2ODcxMjYyMjE4.YQQH8w.DC5zPrKTk8X6i8HpjGqVUik0N3E')
 # client.run('OTEyMzc2NzUwODYyODkzMTI2.YZvDEA.kXZqdfhiFxBxYw5P8lFjcZ-ZWsQ')
